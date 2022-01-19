be4b9db15c508cdfb2621ce615ff220ca1ec1316
deng <denglinghua@outlook.com>
2022-01-19 08:48:03 +0800

    add : check data

--- datasource.py
+++ datasource.py
@@ -4,6 +4,45 @@ import os
 
 import pandas as pd
 
+_file_data = {'min_year' : 9999, 'max_year':0, 'seq':1}
+
+def _add_file_data(df:pd.DataFrame, file_path):
+    global _file_data
+    
+    end_mon_day = file_path[-9:-5]
+    start_mon_day = file_path[-14:-10]
+    year = int(file_path[-19:-15])
+    
+    if year < _file_data['min_year']:
+        _file_data['min_year'] = year
+    if year > _file_data['max_year']:
+        _file_data['max_year'] = year
+    
+    max_data_date = df['end_time'].max()
+    min_data_date = df['end_time'].min()
+
+    def check_date(date, year, month_day):
+        return date.strftime('%Y%m%d') == '%s%s' % (year, month_day)
+    
+    result = check_date(min_data_date, year, start_mon_day) and check_date(max_data_date,year, end_mon_day)
+
+    key = '%s%s' % (year, start_mon_day[0:2])
+
+    _file_data[key] = result
+
+    desc = '' if result else '(%s-%s)' % (min_data_date, max_data_date)
+
+    print('load data : [%s:%s:%s]%s' % (_file_data['seq'], key, result, desc))
+    _file_data['seq'] += 1
+
+def _check_data():
+    global _file_data
+    for year in range(_file_data['min_year'], _file_data['max_year'] + 1):
+        for m in range(1, 13):
+            key = '%s%s' % (year, "{:02d}".format(m))
+            if key not in _file_data or not _file_data[key]:
+                print('%s data check failed !' % key)
+
 def __to_date_time(str):
     format = '%Y-%m-%d %H:%M:%S'    
     return datetime.strptime(str, format)
@@ -87,8 +126,10 @@ def load_data(data_dir, debug = False):
     for file in data_files:
         one_file_df = pd.read_excel(file, header=7, names=cols, converters=__col_converts())
         # print(one_file_df)
+        _add_file_data(one_file_df, file)
         df = df.append(one_file_df, ignore_index=True)
     
+    _check_data()
 
     df['year'] = df['end_time'].dt.year
     df['month'] = df['end_time'].dt.to_period('M')
