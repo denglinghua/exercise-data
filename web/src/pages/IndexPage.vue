6a620533677520df06d85e8f0a9c7119154dd5d8
deng <denglinghua@outlook.com>
2024-06-16 08:54:53 -0300

    add hint

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,9 +1,17 @@
 <template>
   <q-page>
     <div class="fullscreen q-pa-xs flex flex-center">
-      <q-input v-model="id" label="悦跑圈ID" type="number" @keyup.enter="goDataPage" style="width:200px">
+      <q-input v-model="id" label="悦跑圈ID" type="number" @keyup.enter="goDataPage" style="width:200px" autofocus
+        bottom-slots>
         <template v-slot:append>
           <q-icon name="arrow_forward" size="md" color="primary" @click="goDataPage" />
+        </template>
+        <template v-slot:hint>
+          <q-icon class="q-mr-md" color="grey-6" size="1.2em" name="help_outline">
+            <q-tooltip>
+              悦跑圈APP“我”页面可以查看复制ID
+            </q-tooltip>
+          </q-icon>
         </template>
       </q-input>
     </div>
